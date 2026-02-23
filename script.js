fd1d70ad7fe1fdf95b1ad571bdaaa1a9828b1301
Pokimonstr <semespolina@gmail.com>
2026-02-23 22:00:39 +0300

    Mimi

---
+++ script.js
@@ -0,0 +1,18 @@
+//ARRAYS
+let arr  = [1,3,4,5,6,7];
+console.log(arr.pop());// Удаляет и возвращает последний элемент
+console.log(arr.push(3,5,6,7,98,3));//
+console.log(arr.shift());
+console.log(arr.unshift(100));
+console.log(arr.join());
+console.log(arr.splice(0,3));
+console.log(arr)
+//Browser API
+let name  = prompt('Введите имя','кто-то');
+console.log(name);
+alert('HELLO');
+confirm('Вам больше 18?');
+//EXAMPLE
+let namee  = prompt('как тебя зовут?')
+alert('Привет тебе '+namee);
+
